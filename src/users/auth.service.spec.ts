a0a1a182aaad13eca5523815c665948f7661ca24
iahmediibrahim <iahmediibrahim@gmail.com>
2026-05-29 19:52:21 +0300

    test(auth): add auth service tests and update test npm script
    Revise the test npm script to include --watch and --maxWorkers=1, removing the separate test:watch command. Add comprehensive unit tests for AuthService covering service instantiation and proper password salting and hashing during signup.

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(async () => {
+    // create a fake copy of users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+    service = module.get<AuthService>(AuthService);
+  });
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('test@example.com', '123456');
+    expect(user).toBeDefined();
+    expect(user.password).not.toEqual('123456');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+});
